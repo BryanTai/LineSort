cadfe5591ae74f79d234499b65406d14a61fb3fa
Bryan Tai <redhatpieman@gmail.com>
2017-09-11 10:57:08 -0700

    Persons and Lineups can be selected

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : Selectable {
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked Person!");
+        isSelected = true;
+    }
+}
---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Selectable : MonoBehaviour {
+
+    protected Collider2D myCollider;
+    protected bool isSelected;
+
+    // Use this for initialization
+    void Start()
+    {
+        myCollider = GetComponent<Collider2D>();
+        isSelected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
