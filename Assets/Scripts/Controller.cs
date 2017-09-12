f6119f0dffa7a1f0c1bf2f1e480ed45de0522c99
Bryan Tai <redhatpieman@gmail.com>
2017-09-12 10:54:11 -0700

    PersonGenerator instantiates Persons, at least for testing

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,12 +6,15 @@ public class Controller : MonoBehaviour {
 
     Selectable currentlySelected;
     Selectable previouslySelected;
+    
 
     public List<GameObject> AllSelectableObjects;
+    public PersonGenerator personGenerator;
 
 	// Use this for initialization
 	void Start () {
         AllSelectableObjects = new List<GameObject>();
+        personGenerator.Activate();
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/PersonGenerator.cs
+++ Assets/Scripts/PersonGenerator.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PersonGenerator : MonoBehaviour {
 
+    //TODO How to count time?
+    public GameObject person;
+    const int MAX_PERSONS = 2;
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +17,15 @@ public class PersonGenerator : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void Activate()
+    {
+        //TODO For now, just initializes two Persons
+        for (int i = 0; i < MAX_PERSONS; i++)
+        {
+            Vector3 newPosition = new Vector3(i, -1, 0);
+            Instantiate(person, newPosition, Quaternion.identity);
+        }
+    }
+
 }
