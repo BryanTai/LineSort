d80d659a5bfd84178579686b6df18de2bc37e7c9
Bryan Tai <redhatpieman@gmail.com>
2017-09-21 10:46:48 -0700

    New Persons added to GameController List, fixed lineup box colliders

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    Selectable currentlySelected;
+    Selectable previouslySelected;
+    
+
+    public List<GameObject> AllSelectableObjects; //TODO split into list of persons and lineups?
+    public PersonGenerator personGenerator;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(gameObject.tag);
+        AllSelectableObjects = new List<GameObject>();
+        personGenerator.Activate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void TestPublic()
+    {
+        Debug.Log("Another class can access GameController!");
+    }
+
+    public void AddPersonToList(GameObject newPerson)
+    {
+        //TODO Might need a lock on this later
+        AllSelectableObjects.Add(newPerson);
+    }
+}
--- Assets/Scripts/PersonGenerator.cs
+++ Assets/Scripts/PersonGenerator.cs
@@ -7,16 +7,12 @@ public class PersonGenerator : MonoBehaviour {
     //TODO How to count time?
     public GameObject person;
     const int MAX_PERSONS = 2;
+    public GameController gameController;
     
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    }
 
     public void Activate()
     {
@@ -24,7 +20,8 @@ public class PersonGenerator : MonoBehaviour {
         for (int i = 0; i < MAX_PERSONS; i++)
         {
             Vector3 newPosition = new Vector3(i, -1, 0);
-            Instantiate(person, newPosition, Quaternion.identity);
+            GameObject newPerson = Instantiate(person, newPosition, Quaternion.identity);
+            gameController.AddPersonToList(newPerson);
         }
     }
 
