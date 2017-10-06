cf6f9efa77579459f6cc39db4e7aa7619d45f815
Bryan Tai <redhatpieman@gmail.com>
2017-10-06 11:28:10 -0700

    Rules are fully random... I really should write some unit tests

--- Assets/Scripts/RuleGenerator.cs
+++ Assets/Scripts/RuleGenerator.cs
@@ -4,35 +4,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RuleGenerator {
+    System.Random rnd;
 
-	public RuleGenerator()
+    public const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public RuleGenerator()
     {
-
+        rnd = new System.Random();
     }
 
     public Rule GenerateNewRule()
     {
         //TODO perhaps cache the generated Rules so the same ones don't get picked?
-        Rule toReturn = new Rule(pickRuleType(),pickEquality(),pickAmount());
-
-        return toReturn;
+        RuleType nextRule = pickRuleType();
+        switch (nextRule)
+        {
+            case RuleType.CONTAINS:
+            case RuleType.STARTS:
+            case RuleType.ENDS:
+                return new Rule(ruleType: nextRule, firstWord: pickOneLetter());
+            case RuleType.VOWELS:
+            case RuleType.CONSONENTS:
+            case RuleType.LENGTH:
+                return new Rule(ruleType: nextRule, equality: pickEquality(), amount: pickAmount());
+            default:
+                throw new ArgumentException();
+        }
     }
+
+    
 
     private RuleType pickRuleType()
     {
-        //TODO for testing, use random one later
-        return RuleType.LENGTH; 
+        int totalTypes = Enum.GetNames(typeof(RuleType)).Length;
+        int next = rnd.Next(totalTypes);
+        return (RuleType)next;
     }
 
     private Equality pickEquality()
     {
-        //TODO for testing, use random one later
-        return Equality.EQUAL;
+        int totalTypes = Enum.GetNames(typeof(Equality)).Length;
+        int next = rnd.Next(totalTypes);
+        return (Equality)next;
     }
 
     private int pickAmount()
     {
-        //TODO for testing, use random one later
-        return 5;
+        return rnd.Next(1,8); //TODO might need to tweak this
+    }
+
+    private string pickOneLetter()
+    {
+        int nextIndex = rnd.Next(26);
+        return ALPHABET[nextIndex].ToString();
     }
 }
