9912924df8f5d5242517827bd283da82b13eefff
Bryan Tai <redhatpieman@gmail.com>
2017-10-30 11:57:08 -0700

    Scaled StartMenu button to the screen resolution

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuController : MonoBehaviour {
+
+    public Button StartGameButton;
+
+	// Use this for initialization
+	void Start () {
+        float buttonHeight = Screen.height * 0.1f;
+        float buttonWidth = Screen.width * 0.5f;
+        StartGameButton.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, buttonHeight);
+        StartGameButton.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, buttonWidth);
+
+    }
+
+}
